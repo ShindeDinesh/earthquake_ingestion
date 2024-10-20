1c2768ce7f33d0ccd0e57627f819216dcbc2cd3c
ShindeDinesh <shindedinesh557@gmail.com>
2024-10-20 18:23:58 +0530

    Read csv file using spark

--- Bronze/historical_data_pyspark.py
+++ Bronze/historical_data_pyspark.py
@@ -1,1 +1,8 @@
-import pyspark
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.master('local[*]').appName('asd').getOrCreate()
+
+    file=spark.read.csv(r'C:\Users\Win10\Downloads\salespeoples.csv')
+
+    file.show()
