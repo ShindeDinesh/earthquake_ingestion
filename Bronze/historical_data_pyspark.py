8f54029e4cc1c6c88ce42d219d802c754dc08612
ShindeDinesh <shindedinesh557@gmail.com>
2025-01-29 22:34:41 +0530

    remove file

--- Bronze/historical_data_pyspark.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-if __name__ == '__main__':
-    spark = SparkSession.builder.master('local[*]').appName('asd').getOrCreate()
-
-    file=spark.read.csv(r'C:\Users\Win10\Downloads\salespeoples.csv')
-
-    file.show()
